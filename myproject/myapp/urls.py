fe91285c9bdca4d7283d26f4f4e8be168ed7dffc
unknown <sanjeevkumarrai132@gmail.com>
2025-08-31 20:03:50 +0530

    commit done

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Deptview , Empview
+from .views import Deptview , Empview , EmpUpdation
 urlpatterns = [
     path('dept/', Deptview.as_view(), name='dept'),
     path('emp/', Empview.as_view(), name='emp'),
+    path('emp/<int:emp_id>/' , EmpUpdation.as_view(), name='EmpUpdation')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -41,3 +41,35 @@ class Empview(APIView):
         else:
             print(serializer.errors)
             return Response({'error': 'Employee creation failed', 'details': serializer.errors}, status=400)
+        
+class EmpUpdation(APIView):
+    def get(self,request,emp_id):
+        try:
+            emp = Employee.objects.get(emp_id=emp_id)
+            serlizer = Empserializer(emp)
+            return Response(serlizer.data)
+        except Exception as e:
+            return Response({'error': 'Employee not found'}, status=404)
+    #update the employee details
+    def put(self , request , emp_id):
+        try:
+            emp = Employee.objects.get(emp_id=emp_id)
+            payload = request.data
+            serializer = Empserializer(emp, data=payload , partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response({'error': 'Invalid data', 'details': serializer.errors}, status=400)
+        except Exception as e:
+            return Response({'error':"employee not found"})
+        
+    def delete(self , request , emp_id):
+        try:
+            print('Deleting employee with ID:', emp_id)
+            emp = Employee.objects.get(emp_id = emp_id)
+            print('******************', emp)
+            emp.status = False
+            emp.save()
+            return Response({'success': 'Employee deleted successfully'})
+        except Exception as e:
+            return Response({'error':"employee not found"})
