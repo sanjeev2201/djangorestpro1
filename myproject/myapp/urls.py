a09f5dfd98b9b573f659f268ddcc2d7650999dcd
unknown <sanjeevkumarrai132@gmail.com>
2025-08-31 12:21:34 +0530

    commit done

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import Deptview
+from .views import Deptview , Empview
 urlpatterns = [
     path('dept/', Deptview.as_view(), name='dept'),
+    path('emp/', Empview.as_view(), name='emp'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from .models import Department , Employee
 from .serializer import Deptserializer , Empserializer
 # Create your views here.
-
+from datetime import date ,datetime
 class Deptview(APIView):
     def get(self , request):
         dept = Department.objects.filter(status=True).all().order_by('dept_id')
@@ -16,4 +16,28 @@ class Deptview(APIView):
         serializer  = Deptserializer(data=payload)
         if serializer.is_valid():
             serializer.save()
-        return Response({'employee added successfully !!!'})
+        return Response({'department added successfully !!!'})
+    
+class Empview(APIView):
+    def get(self , request):
+        emp = Employee.objects.filter(status=True).all().order_by('emp_id')
+        # empdata =Employee.objects.filter(department=1).all()
+        # for i in empdata:
+        #     print('empdt:>>>>>>>>>>>>>>>>>',i.department.dept_name)
+        # emp = Employee.objects.all()
+        print(emp)
+        serializer  = Empserializer(emp , many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        payload = request.data
+        payload['status'] = True
+        payload['hire_date'] = date.today()
+        serializer = Empserializer(data = payload)
+        if serializer.is_valid():
+            print('save in progress...')
+            serializer.save()
+            return Response(serializer.data)
+            # return Response({'employee added successfully !!!'})
+        else:
+            print(serializer.errors)
+            return Response({'error': 'Employee creation failed', 'details': serializer.errors}, status=400)
